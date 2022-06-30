94bbc61728b486feed835c7ee923f42f08eca909
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-06-30 12:39:36 +0530

    debouncing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+
+  let [input, setinput] = useState("");
+  let id = useRef();
+
+  let [data, setdata] = useState([]);
+
+  async function getdata() {
+    console.log("yes")
+    let x = await fetch(`http://localhost:3000/country?q=${input}`);
+    let y = await x.json();
+    console.log(y)
+    setdata(y)
+
+  }
+
+  useEffect(() => {
+
+
+    if (input.length) {
+
+      clearTimeout(id.current)
+
+
+      id.current = setTimeout(() => {
+        getdata()
+      }, 1000);
+
+
+    }
+
+
+
+
+
+
+  }, [input])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="input_div">
+        <div ><input type="text" onChange={(e) => { setinput(e.target.value) }} /></div>
+
+        <div className="hide_scroll" style={{ overflow: "hidden", scrollbarWidth: "10px" }}>
+
+          {data.map((e, index) => <div key={index} style={{ margin: "10px", textAlign: "start" }}>{e.country}</div>)}
+
+        </div>
+      </div>
+    </div >
   );
 }
 
